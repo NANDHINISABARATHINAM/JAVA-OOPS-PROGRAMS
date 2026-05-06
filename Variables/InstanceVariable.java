8fe11d75837f041f1a70ee380ff28027082ff59d
NANDHINI_S02 <112407334+NANDHINISABARATHINAM@users.noreply.github.com>
2026-05-06 19:11:47 +0530

    Update InstanceVariable.java

--- Variables/InstanceVariable.java
+++ Variables/InstanceVariable.java
@@ -1,8 +1,10 @@
 public class Main
 {
     int a=100;//instance variable
+    int b;
 	public static void main(String[] args) {
 		Main ob=new Main();
-		System.out.println(ob.a);
+		System.out.println(ob.a); //100
+		System.out.println(ob.b); //0
 	}
 }
