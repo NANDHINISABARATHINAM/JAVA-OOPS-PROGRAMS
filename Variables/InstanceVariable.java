0513149326f397a16ec6cfc1b24756532b931a7f
NANDHINI_S02 <112407334+NANDHINISABARATHINAM@users.noreply.github.com>
2026-05-06 19:24:11 +0530

    Update InstanceVariable.java

--- Variables/InstanceVariable.java
+++ Variables/InstanceVariable.java
@@ -2,9 +2,17 @@ public class Main
 {
     int a=100;//instance variable
     int b;
+	void fun(){
+        System.out.println(a);
+    }
+    void fun(int n){
+        System.out.println(n);
+    }
 	public static void main(String[] args) {
 		Main ob=new Main();
 		System.out.println(ob.a); //100
 		System.out.println(ob.b); //0
+		ob.fun(); //100
+		ob.fun(10); //10
 	}
 }
