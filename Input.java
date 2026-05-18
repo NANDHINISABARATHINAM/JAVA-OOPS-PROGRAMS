2d90423e4ba182fcef8a97cb0be7a79914286996
NANDHINI_S02 <112407334+NANDHINISABARATHINAM@users.noreply.github.com>
2026-05-18 15:31:58 +0530

    Create Input.java

---
+++ Input.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter value:");
+        String s=sc.next();
+        System.out.println(s);
+        String s1= sc.nextLine();
+        System.out.println(s1);
+        char ch=sc.next().charAt(0);
+		System.out.println(ch);
+		int a=sc.nextInt();
+        System.out.println(a);
+        float f= sc.nextFloat();
+        System.out.println(f);
+        boolean b=sc.nextBoolean();
+        System.out.println(b);
+	}
+}
